7a4fce17d082a73dd2714ad94fd89b6149e1b2c9
Michael Gillam <jamargillam@gmail.com>
2017-09-10 13:28:24 -0500

    Fixed CORS issue

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 var restify = require('restify');
 var server = restify.createServer();
+var corsMiddleware = require('restify-cors-middleware');
 
 function respond(req, res, next) {
-  const email = req.params.email;
+  const email = req.body.email;
   const respObj = {
     'message': `Subscribed using ${email}`
-  }
-  res.send(200, respObj);
+  };
+  res.send(respObj);
   next();
 }
 
+var cors = corsMiddleware({
+  origins: ['http://localhost:3000']
+});
 server.use(restify.plugins.bodyParser());
+server.pre(cors.preflight);
+server.use(cors.actual);
+
 server.post('/subscribe', respond);
 
 server.listen(8080, function () {
